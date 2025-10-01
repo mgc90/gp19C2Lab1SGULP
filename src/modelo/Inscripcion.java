5e8b155b3ef3d09f3c565e9e6ef314c39b9994e0
Valeria Muñoz Olguin <valeriaselenamunozolguin@gmail.com>
2025-10-01 16:39:59 -0300

    Creación Clases Materia e Inscripción

---
+++ src/modelo/Inscripcion.java
@@ -0,0 +1,49 @@
+
+package modelo;
+
+
+public class Inscripcion {
+    private int idInscripto;
+    private int nota;
+    private int idAlumno;
+    private int idMateria;
+
+    public Inscripcion(int idInscripto, int nota, int idAlumno, int idMateria) {
+        this.idInscripto = idInscripto;
+        this.nota = nota;
+        this.idAlumno = idAlumno;
+        this.idMateria = idMateria;
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+}
---
+++ src/modelo/Materia.java
@@ -0,0 +1,51 @@
+
+package modelo;
+
+
+public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int anio;
+    private boolean estado;
+
+    public Materia(int idMateria, String nombre, int anio, boolean estado) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+}
